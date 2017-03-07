3527242cd029d82cfee3a9aedaa5145c20d1d186
katherine tuter <katherinetuter@gmail.com>
2017-03-06 19:56:37 -0600

    added json data

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-console.log("hello");
+function executeThisCodeAfterFileLoaded(){
+	console.log("it worked!!")
+}
+
+function executeThisCodeAfterFileFails(){
+	console.log("it failed!!")
+}
 
 
+var myRequest = new XMLHttpRequest();
+//have line above for each request; var must be different name each time
+
+myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+myRequest.addEventListener("error", executeThisCodeAfterFileFails);
+myRequest.open("GET", "dinosaurs.json");
+myRequest.send();
+console.log("myRequest", myRequest);
